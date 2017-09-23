c5462e7b03bcf692c5c3d5be386e7b534a92100f
Serhii Shepel <serhii.shepel@gmail.com>
2017-09-23 19:43:20 +0300

    Add desc in property.xml

--- src/com/kony/appfactory/visualizer/channels/SpaChannel.groovy
+++ src/com/kony/appfactory/visualizer/channels/SpaChannel.groovy
@@ -5,13 +5,45 @@ import com.kony.appfactory.helper.BuildHelper
 
 class SpaChannel extends Channel {
     /* Build parameters */
-    String publishFabricApp
+    Boolean publishFabricApp
+    String fabricAppName
+    String fabricAccountId
 
     SpaChannel(script) {
         super(script)
         nodeLabel = 'win || mac'
-        publishFabricApp = this.script.env.PUBLISH_FABRIC_APP
+        publishFabricApp = this.script.params.PUBLISH_FABRIC_APP
+        fabricAppName = this.script.env.FABRIC_APP_NAME
+        fabricAccountId = this.script.env.FABRIC_ACCOUNT_ID
         channelVariableName = channelPath = 'SPA'
+    }
+
+    private final fetchFabricCli() {
+        String mfCliUrl = 'https://s3-eu-west-1.amazonaws.com/konyappfactorydev-ci0001-storage1/configuration/mf/mfcli.jar'
+        String mfCliUrlFileName = 'mfcli.jar'
+
+        script.catchErrorCustom('FAILED to fetch MF CLI!') {
+            script.httpRequest url: mfCliUrl, outputFile: mfCliUrlFileName, validResponseCodes: '200'
+        }
+
+    }
+
+    private final mfCLI(args) {
+        String command = args.command
+        String options = args.options
+        String mfCredID = args.mfCredID
+        String successMessage = String.valueOf('publish'.capitalize()) + ' finished successfully'
+        String errorMessage = 'FAILED to run ' + String.valueOf('publish') + ' command'
+
+        script.catchErrorCustom(successMessage, errorMessage) {
+            script.withCredentials([[$class          : 'UsernamePasswordMultiBinding',
+                                     credentialsId   : mfCredID,
+                                     passwordVariable: 'mfPassword',
+                                     usernameVariable: 'mfUser']]) {
+                script.shellCustom("java -jar mfcli.jar \"${command}\" -u \"${script.env.mfUser}\" \
+                    -p \"${script.env.mfPassword}\" ${options}", isUnixNode)
+            }
+        }
     }
 
     protected final void createPipeline() {
@@ -34,13 +66,17 @@ class SpaChannel extends Channel {
                             script.error('Build artifacts were not found!')
                 }
 
-                script.stage('Publish to Fabric') {
-                    if (publishFabricApp) {
-                        script.echo 'In progress...'
-                    } else {
-                        script.echo 'Ignoring!'
-                    }
-                }
+//                script.stage('Publish to Fabric') {
+//                    if (publishFabricApp) {
+//                        script.echo 'In progress...'
+//                        fetchFabricCli()
+//                        String publishOptions = "-t \"${fabricAccountId}\" -a \"${fabricAppName}\" -e \"App Factory Dev\""
+//
+//                        mfCLI command: 'publish', options: publishOptions, mfCredID: cloudCredentialsID
+//                    } else {
+//                        script.echo 'Ignoring!'
+//                    }
+//                }
 
                 script.stage("Publish artifacts to S3") {
                     /* Rename artifacts for publishing */
