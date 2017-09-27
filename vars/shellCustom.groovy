d895969ee5a4187d1263e4614df54648a197557a
Serhii Shepel <serhii.shepel@gmail.com>
2017-09-27 10:44:05 +0300

    Change shell parameters for unix nodes

--- vars/shellCustom.groovy
+++ vars/shellCustom.groovy
@@ -1,7 +1,7 @@
 def call(String shellScript, Boolean isUnixNode = true, Map args = [:]) {
     String shellType = (isUnixNode) ? 'sh' : 'bat'
 
-    "$shellType" script: ((!isUnixNode) ? shellScript : '#!/bin/sh -e\n' + shellScript),
+    "$shellType" script: shellScript,
             returnStatus: (args.returnStatus) ?: false,
             returnStdout: (args.returnStdout) ?: false
 }
