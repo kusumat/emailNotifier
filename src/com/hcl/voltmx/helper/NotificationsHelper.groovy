0b755291ce8b3a585c2721ed4fe5dbe8a47f1c43
kusumat <kusum.sam@gmail.com>
2022-05-23 20:41:46 +0530

    fixes

--- src/com/hcl/voltmx/helper/NotificationsHelper.groovy
+++ src/com/hcl/voltmx/helper/NotificationsHelper.groovy
@@ -122,7 +122,7 @@ class NotificationsHelper implements Serializable {
                         number  : script.currentBuild.number,
                         result  : script.currentBuild.currentResult,
                         url     : script.env.BUILD_URL,
-                        branch  : script.BRANCH_NAME,
+                        branch  : script.env.BRANCH_NAME,
                         started : script.currentBuild.rawBuild.getTime().toLocaleString() + ' ' +
                                 System.getProperty('user.timezone').toUpperCase(),
                         log     : script.currentBuild.rawBuild.getLog(100),
