22f29a153edad19183a932644a73af7b8c28c54c
kusumat <kusum.sam@gmail.com>
2022-05-26 01:32:57 +0530

    change log

--- src/com/hcl/voltmx/helper/EmailBuilder.groovy
+++ src/com/hcl/voltmx/helper/EmailBuilder.groovy
@@ -58,40 +58,23 @@ class EmailBuilder {
             }
         }
     }
-    
+
     @NonCPS
     static void addScmTableRow(htmlBuilder, scmMeta) {
-        def channelList = scmMeta.keySet()
-        /* This map will hold the channels having same commitID as :- {commitID(key) :[channelsList](value)} */
-        Map<String, List<String>> commitIdMapForChannels = new HashMap<String, List<String>>()
-        for (channel in channelList) {
-            def commidID = scmMeta[channel].commitID
-            commitIdMapForChannels.putIfAbsent(commidID, new ArrayList<String>())
-            commitIdMapForChannels.get(commidID).add(channel)
-        }
-        def commitIdList = commitIdMapForChannels.keySet()
-
-        commitIdList.each { id ->
-            commitIdMapForChannels[id].eachWithIndex { channel, index ->
-                htmlBuilder.tr {
-                    th(channel.replaceAll('/', ' '))
-                    if (scmMeta[channel] && scmMeta[channel].scmUrl && scmMeta[channel].commitID && scmMeta[channel].commitLogs) {
-                        if (index == 0) {
-                            td(style: "text-align:center; border-right: 1px solid #e8e8e8; width: 65px", rowspan: commitIdMapForChannels[id].size()) {
-                                String commitIdUrl = scmMeta[channel].scmUrl.replace(".git", "/commit/") + scmMeta[channel].commitID
-                                a(href: commitIdUrl, target: '_blank', scmMeta[channel].commitID.substring(0, 7))
-                            }
-                            td(style: "border-right: 1px solid #e8e8e8", rowspan: commitIdMapForChannels[id].size()) {
-                                def logsList = scmMeta[channel].commitLogs
-                                for (def pathIndex = 0; pathIndex < 10 && pathIndex < logsList.size(); pathIndex++)
-                                    p(style: "font-size:12px;", logsList[pathIndex])
-                            }
-                        }
-                    } else {
-                        td(style: "color:red", colspan: "2") {
-                            mkp.yield("Checkout failed")
-                        }
-                    }
+        htmlBuilder.tr {
+            if (scmMeta && scmMeta.scmUrl && scmMeta.commitID && scmMeta.commitLogs) {
+                td(style: "text-align:center; border-right: 1px solid #e8e8e8; width: 65px") {
+                    String commitIdUrl = scmMeta.scmUrl.replace(".git", "/commit/") + scmMeta.commitID
+                    a(href: commitIdUrl, target: '_blank', scmMeta.commitID.substring(0, 7))
+                }
+                td(style: "border-right: 1px solid #e8e8e8") {
+                    def logsList = scmMeta.commitLogs
+                    for (def pathIndex = 0; pathIndex < 10 && pathIndex < logsList.size(); pathIndex++)
+                        p(style: "font-size:12px;", logsList[pathIndex])
+                }
+            } else {
+                td(style: "color:red", colspan: "2") {
+                    mkp.yield("Checkout failed")
                 }
             }
         }
