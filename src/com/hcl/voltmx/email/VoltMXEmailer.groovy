0eb1312625a1d8108f34e89982ebcf2b1ab0b0e0
kusumat <kusum.sam@gmail.com>
2022-06-01 10:16:33 +0530

    changed branch name

--- src/com/hcl/voltmx/email/VoltMXEmailer.groovy
+++ src/com/hcl/voltmx/email/VoltMXEmailer.groovy
@@ -39,7 +39,7 @@ class VoltMXEmailer implements Serializable {
                                 def paramsList = ['Branch_Name','GIT_BRANCH']
                                 def branchName = BuildHelper.getParamNameOrDefaultFromProbableParamList(script, paramsList, 'BRANCH_NAME')
                                 script.echoCustom("branch param is $branchName",'INFO')
-                                String branch = script.params.branchName
+                                String branch = script.params.BRANCH_NAME
                                 script.echoCustom("branch  is $branch",'INFO')
                                 String credentialID = 'c401aa36-3cb9-4849-ad29-ee79196bd286'
                                 String repoURL = script.env.REPO_URL
