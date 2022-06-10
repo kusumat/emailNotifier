fcc7f1893ad5b743d4969b6d09237268e5217b09
kusumat <kusum.sam@gmail.com>
2022-06-10 13:21:21 +0530

    sample env

--- src/MyClass.groovy
+++
@@ -1,3 +0,0 @@
-public MyClass(def script) {
-    throw new Exception("FOO=${script.env.FOO}")
-}
--- var/lib.groovy
+++
@@ -1,4 +0,0 @@
-def displayEnv(script) {
-    println "Shared lib var/lib FOO=${FOO}"
-    MyClass c = new MyClass(script);
-}
