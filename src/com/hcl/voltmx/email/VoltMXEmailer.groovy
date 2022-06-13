f150a7d56396396fd4c5315b3d6ef357264a52fe
kusumat <kusum.sam@gmail.com>
2022-06-13 11:26:08 +0530

    sample env

--- src/com/hcl/voltmx/email/VoltMXEmailer.groovy
+++ src/com/hcl/voltmx/email/VoltMXEmailer.groovy
@@ -36,9 +36,10 @@ class VoltMXEmailer implements Serializable {
                                 def varsList = []
                                 varsList = script.env.varslist
                                 script.echoCustom("vars "+ varsList)
-                                script.echoCustom("kms "+ script.env.kmsvars)
+                                script.echoCustom("kms "+ script.evaluate(script.env.kmsvars))
                                 script.echoCustom("tenant "+ script.env.tenantvars)
                                 script.echoCustom("ten "+ varsList)
+
 //                                script.env.kmsvars.each {
 //                                    script.echoCustom ("${it.key} = ${it.value}\n")
 //                                }
