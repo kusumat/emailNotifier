82ee2d5fd291a902af9dfa794b2702924a420448
kusumat <kusum.sam@gmail.com>
2022-06-13 16:37:04 +0530

    sample env

--- src/com/hcl/voltmx/email/VoltMXEmailer.groovy
+++ src/com/hcl/voltmx/email/VoltMXEmailer.groovy
@@ -46,8 +46,10 @@ class VoltMXEmailer implements Serializable {
                              def kmsmap = script.env.varmap
                               String value = kmsmap
                                 script.echoCustom("kmsmap "+ value)
-                                value = value.substring(1, value.length()-1);           //remove curly brackets
-                                String[] keyValuePairs = value.split(",");              //split the string to creat key-value pairs
+                                value = value.substring(1, value.length()-1);
+                                script.echoCustom("value is "+ value)
+                                //remove curly brackets
+                                String[] keyValuePairs = value.split(";");              //split the string to creat key-value pairs
                                 Map<String,Map<String,String>> map = new HashMap<>();
 
                                 for(String pair : keyValuePairs)                        //iterate over the pairs
@@ -55,6 +57,7 @@ class VoltMXEmailer implements Serializable {
                                     String[] entry = pair.split("=");                   //split the pairs to get key and value
                                     map.put(entry[0].trim(), entry[1].trim());          //add them to the hashmap and trim whitespaces
                                 }
+                                script.echoCustom("map size"+map.size())
                                 map.each {
                                     script.echoCustom ("${it.key} = ${it.value}\n")
                                 }
